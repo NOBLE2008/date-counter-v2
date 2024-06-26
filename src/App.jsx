e91d7448eb9c9c39d8b6ff0bc4577bd1e2fa0cc2
Noble Benjamin <nobleben2008@gmail.com>
2024-06-26 19:43:10 +0100

    Made the needed state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  
+  const [count, setCount] = useState(Date.now())
+
+  const [steps, setSteps] = useState(1)
+
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
   return (
     <div>
-      <button>-</button>
-      <p>Steps: X</p>
+      <button></button>
+      <p>Steps: {steps}</p>
       <button>+</button>
       <button>-</button>
       <p>Count</p>
       <button>+</button>
-      <p>X days ago was </p>
+      <p></p>
     </div>
   )
 }
